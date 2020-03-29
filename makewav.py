c06b9129a97fdcc9cb54415968446cfd17d382fe
Erwin Bonsma <eriban@users.sourceforge.net>
2020-03-29 15:10:53 +0200

    Support 8-bit samples

--- makewav.py
+++ makewav.py
@@ -3,10 +3,12 @@ import wave
 import struct
 from functools import partial
 
-data_size = 40000
+sampwidth = 2
+duration = 3 # in seconds
 fname = "WaveTest.wav"
 frate = 11025.0  # framerate as a float
-amp = 16000.0    # multiplier for amplitude
+amp = 84 * 256**(sampwidth - 1) # multiplier for amplitude
+data_size = int(frate * duration)
 
 freqs = [
     261.63, # C
@@ -14,14 +16,20 @@ freqs = [
     392.00  # G
 ]
 
+# Normalized frequency such that period is an integer-number of samples
+nfreqs = [
+    frate / round(frate / freq) for freq in freqs
+]
+
 def sine_wave(x, freq):
     return math.sin(2 * math.pi * freq * (x/frate))
 
-funs = [
-    partial(sine_wave, freq=freq) for freq in freqs
-]
+def square_wave(x, freq):
+    return math.copysign(1, sine_wave(x, freq))
 
-print("#funs = %d" % (len(funs)))
+funs = [
+    partial(sine_wave, freq=freq) for freq in nfreqs
+]
 
 sine_list_x = []
 for x in range(data_size):
@@ -31,17 +39,18 @@ for x in range(data_size):
 wav_file = wave.open(fname, "w")
 
 nchannels = 1
-sampwidth = 2
 framerate = int(frate)
 nframes = data_size
 comptype = "NONE"
 compname = "not compressed"
 
-wav_file.setparams((nchannels, sampwidth, framerate, nframes,
-    comptype, compname))
+wav_file.setparams((nchannels, sampwidth, framerate, nframes, comptype, compname))
 
 for s in sine_list_x:
     # write the audio frames to file
-    wav_file.writeframes(struct.pack('h', int(s*amp/2)))
+    if sampwidth == 1:
+        wav_file.writeframes(struct.pack('B', 128 + int(s*amp/2)))
+    else:
+        wav_file.writeframes(struct.pack('h', int(s*amp/2)))
 
 wav_file.close()
