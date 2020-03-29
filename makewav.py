4d5db2a28c3d7fdca84da5c683d7f22366544376
Erwin Bonsma <eriban@users.sourceforge.net>
2020-03-29 16:59:03 +0200

    Make output range more configurable
    Make it possible to specify number of steps. Not only 256 (max for 8-bit) and 65536 (max for 16-bit) but also for example 1024 (range of DAC in GameBuino).

--- makewav.py
+++ makewav.py
@@ -3,11 +3,13 @@ import wave
 import struct
 from functools import partial
 
-sampwidth = 2
+sampwidth = 1
 duration = 3 # in seconds
 fname = "WaveTest.wav"
 frate = 11025.0  # framerate as a float
-amp = 84 * 256**(sampwidth - 1) # multiplier for amplitude
+output_steps = 256 # 1024
+stepsize = int(256**sampwidth / output_steps)
+input_max = 3 # Maximum value of input (it will be clipped when it exceeds these bounds)
 data_size = int(frate * duration)
 
 freqs = [
@@ -27,14 +29,20 @@ def sine_wave(x, freq):
 def square_wave(x, freq):
     return math.copysign(1, sine_wave(x, freq))
 
+def make_discrete(fval):
+    clipped_val = min(max(fval, -input_max), input_max)
+    return stepsize * math.floor(0.5 * (clipped_val / input_max) * output_steps)
+
 funs = [
-    partial(sine_wave, freq=freq) for freq in nfreqs
+    partial(sine_wave, freq=freq) for freq in freqs
 ]
 
-sine_list_x = []
+raw = []
 for x in range(data_size):
-    s = sum([fun(x) for fun in funs])
-    sine_list_x.append(s)
+    summed = sum([fun(x) for fun in funs])
+    stepped = make_discrete(summed)
+    print("%f -> %d" % (summed, stepped))
+    raw.append(stepped)
 
 wav_file = wave.open(fname, "w")
 
@@ -46,11 +54,11 @@ compname = "not compressed"
 
 wav_file.setparams((nchannels, sampwidth, framerate, nframes, comptype, compname))
 
-for s in sine_list_x:
+for output in raw:
     # write the audio frames to file
     if sampwidth == 1:
-        wav_file.writeframes(struct.pack('B', 128 + int(s*amp/2)))
+        wav_file.writeframes(struct.pack('B', 128 + output))
     else:
-        wav_file.writeframes(struct.pack('h', int(s*amp/2)))
+        wav_file.writeframes(struct.pack('h', output))
 
 wav_file.close()
