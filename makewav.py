90d50cc469d5ecb045b4a825b37c2f2db4ca271e
Erwin Bonsma <eriban@users.sourceforge.net>
2020-03-29 14:51:13 +0200

    Let it generate a chord

--- makewav.py
+++ makewav.py
@@ -1,16 +1,32 @@
 import math
 import wave
 import struct
+from functools import partial
 
-freq = 440.0
 data_size = 40000
 fname = "WaveTest.wav"
 frate = 11025.0  # framerate as a float
-amp = 64000.0     # multiplier for amplitude
+amp = 16000.0    # multiplier for amplitude
+
+freqs = [
+    261.63, # C
+    329.63, # E
+    392.00  # G
+]
+
+def sine_wave(x, freq):
+    return math.sin(2 * math.pi * freq * (x/frate))
+
+funs = [
+    partial(sine_wave, freq=freq) for freq in freqs
+]
+
+print("#funs = %d" % (len(funs)))
 
 sine_list_x = []
 for x in range(data_size):
-    sine_list_x.append(math.sin(2*math.pi*freq*(x/frate)))
+    s = sum([fun(x) for fun in funs])
+    sine_list_x.append(s)
 
 wav_file = wave.open(fname, "w")
 
