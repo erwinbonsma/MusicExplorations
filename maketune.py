1176e0af5971492b522c370e8a5e1e43ada7a8af
Erwin Bonsma <eriban@users.sourceforge.net>
2020-04-01 23:19:49 +0200

    Add FADE_IN and SLIDE effects and conditional ramp up and down
    Ramp up and down of volume at start/end of note now only happens when the wave form changes.
    Also, when the wave from remains the same, also ensured that there is no abrupt transition in the wave form. This is even the case when the volume or frequency changes.
    
    Also added first version of SFX18. Still missing, a vibrato effect.

--- maketune.py
+++ maketune.py
@@ -14,9 +14,10 @@ max_volume = 7
 wave_table_size = 256
 
 # Number of samples per volume step to ramp up (down) the volume at start (end) of a note
-border_samples = 16
+ramp_up_samples = 16
+ramp_dn_samples = ramp_up_samples
 
-samples_per_note = int(frate / 7.5)
+default_samples_per_note = int(frate / 7.5)
 
 triangle_table = []
 triangle_table.extend(range(min_val, max_val))
@@ -54,7 +55,10 @@ note_freqs = {
 
 class Effect(IntEnum):
     NONE = 0
+    SLIDE = 1
+    VIBRATO = 2
     DROP = 3
+    FADE_IN = 4
     FADE_OUT = 5
 
 class Wave(IntEnum):
@@ -64,6 +68,24 @@ class Wave(IntEnum):
     NOISE = 8
 
 silence = ("", 0, Wave.SILENCE, 0, Effect.NONE)
+
+sfx_test1 = [
+    # Four equal notes. These should fully blend into one.
+    ("C", 3, Wave.TRIANGLE, 5, Effect.NONE),
+    ("C", 3, Wave.TRIANGLE, 5, Effect.NONE),
+    ("C", 3, Wave.TRIANGLE, 5, Effect.NONE),
+    ("C", 3, Wave.TRIANGLE, 5, Effect.NONE),
+    silence,
+    ("C", 3, Wave.TRIANGLE, 5, Effect.NONE),
+    ("D", 3, Wave.TRIANGLE, 5, Effect.NONE),
+    ("E", 3, Wave.TRIANGLE, 5, Effect.NONE),
+    ("F", 3, Wave.TRIANGLE, 5, Effect.NONE),
+    silence,
+    ("C", 3, Wave.TRIANGLE, 5, Effect.NONE),
+    ("C", 3, Wave.TRIANGLE, 4, Effect.NONE),
+    ("C", 3, Wave.TRIANGLE, 3, Effect.NONE),
+    ("C", 3, Wave.TRIANGLE, 2, Effect.NONE),
+]
 
 # SFX 17 in Repair.p8
 tune_volume = max_volume - 2
@@ -103,6 +125,39 @@ sfx17 = [
     silence,
     ("F#", 2, Wave.ORGAN, tune_volume - 4, Effect.NONE),
     silence,
+]
+
+sfx18 = [
+    ("G", 3, Wave.TRIANGLE, 4, Effect.NONE),
+    ("G", 3, Wave.TRIANGLE, 4, Effect.NONE),
+    ("G", 3, Wave.TRIANGLE, 4, Effect.NONE),
+    ("G", 3, Wave.TRIANGLE, 4, Effect.VIBRATO),
+    ("G", 3, Wave.TRIANGLE, 4, Effect.VIBRATO),
+    ("G", 3, Wave.TRIANGLE, 4, Effect.VIBRATO),
+    ("G", 3, Wave.TRIANGLE, 4, Effect.VIBRATO),
+    ("G#", 3, Wave.TRIANGLE, 4, Effect.NONE),
+
+    ("G", 3, Wave.TRIANGLE, 4, Effect.NONE),
+    ("G", 3, Wave.TRIANGLE, 4, Effect.VIBRATO),
+    ("G", 3, Wave.TRIANGLE, 4, Effect.VIBRATO),
+    ("G", 3, Wave.TRIANGLE, 4, Effect.VIBRATO),
+    ("D#", 3, Wave.TRIANGLE, 4, Effect.NONE),
+    ("D#", 3, Wave.TRIANGLE, 4, Effect.NONE),
+    ("D#", 3, Wave.TRIANGLE, 4, Effect.VIBRATO),
+    ("C", 3, Wave.TRIANGLE, 4, Effect.NONE),
+
+    ("F#", 3, Wave.TRIANGLE, 4, Effect.NONE),
+    ("F#", 3, Wave.TRIANGLE, 4, Effect.NONE),
+    ("F#", 3, Wave.TRIANGLE, 3, Effect.SLIDE),
+    ("F#", 3, Wave.TRIANGLE, 3, Effect.NONE),
+    ("F#", 3, Wave.TRIANGLE, 2, Effect.SLIDE),
+    ("F#", 3, Wave.TRIANGLE, 2, Effect.NONE),
+    ("F#", 3, Wave.TRIANGLE, 2, Effect.NONE),
+    ("F#", 3, Wave.TRIANGLE, 1, Effect.SLIDE),
+
+    ("F#", 3, Wave.TRIANGLE, 1, Effect.NONE),
+    ("F#", 3, Wave.TRIANGLE, 1, Effect.NONE),
+    ("F#", 3, Wave.TRIANGLE, 1, Effect.FADE_OUT)
 ]
 
 sfx26 = [
@@ -156,37 +211,83 @@ sine_table = [
     make_discrete(math.sin(2 * math.pi * t / wave_table_size)) for t in range(wave_table_size)
 ]
 
-def make_tune(notes):
+def make_tune(notes, samples_per_note = default_samples_per_note):
     samples = []
-    for note in notes:
-        name, octave, wave, volume, effect = note
+
+    for note_idx in range(len(notes)):
+        name, octave, wave, volume, effect = notes[note_idx]
         if wave == Wave.NOISE:
             wave_table = wave_forms[Wave.TRIANGLE]
         else:
             wave_table = wave_forms[wave]
-        t_delta = 0
         if wave == Wave.SILENCE:
             period = 1
         else:
             period = frate / (note_freqs[name] * 2**(octave - 4 + 1))
+
+        volume_start = volume
+        volume_end = volume
+        period_start = period
+        period_end = period
+
         if effect == Effect.DROP:
             period_end = period * 2
-        else:
-            period_end = period
-        start_volume = volume
-        end_volume = volume
-        if effect == Effect.FADE_OUT:
-            end_volume = 0
+        elif effect == Effect.FADE_OUT:
+            volume_end = 0
+        elif effect == Effect.FADE_IN:
+            volume_start = 0
+        elif effect == Effect.SLIDE:
+            volume_start = prev_volume_end
+            period_start = prev_period_end
 
-        for t in range(samples_per_note):
-            p = lerp(period, period_end, t/samples_per_note)
+        prev_wave = None
+        next_wave = None
+        if note_idx > 0:
+            _, _, prev_wave, _, _ = notes[note_idx - 1]
+        if note_idx < len(notes) - 1:
+            _, _, next_wave, _, _ = notes[note_idx + 1]
+
+        if wave == prev_wave:
+            volume_ramp_up = prev_volume_end
+        else:
+            volume_ramp_up = 0
+            t = 0
+
+        if wave == next_wave:
+            volume_ramp_dn = volume_end
+        else:
+            volume_ramp_dn = 0
+
+        t_delta = 0
+
+        for n in range(samples_per_note):
+            p = lerp(period_start, period_end, n / samples_per_note)
+            v = lerp(volume_start, volume_end, n / samples_per_note)
+
             if wave == Wave.NOISE:
-                t_delta += (random.random() - 0.5) * p / 8
-            sample = wave_table[ int((t + t_delta) * len(wave_table) / p) % len(wave_table) ]
-            boundary_delta = min(t, samples_per_note - 1 - t)
-            v = lerp(start_volume, end_volume, t / samples_per_note)
-            v = v * min(border_samples, boundary_delta) / border_samples
+                t_delta += (random.random() - 0.5) * len(wave_table) / 8
+                if t_delta < 0:
+                    t_delta += len(wave_table)
+
+            t += len(wave_table) / p
+            if t >= len(wave_table):
+                t -= len(wave_table)
+
+            if t_delta == 0:
+                sample = wave_table[ int(t) ]
+            else:
+                sample = wave_table[ int(t + t_delta) %  len(wave_table) ]
+
+            if n < ramp_up_samples:
+                v = lerp(volume_ramp_up, v, n / ramp_up_samples)
+            elif n >= samples_per_note - ramp_dn_samples:
+                v = lerp(volume_ramp_dn, v, (samples_per_note - 1 - n) / ramp_dn_samples)
+
             samples.append(int(sample * (v / max_volume)))
+
+        prev_period_end = period_end
+        prev_volume_end = volume_end
+        prev_wave = wave
     return samples
 
 def write_wav(samples, fname):
@@ -205,10 +306,14 @@ def write_wav(samples, fname):
 
     wav_file.close()
 
+samples_test1 = make_tune(sfx_test1)
 samples17 = make_tune(sfx17)
 samples26 = make_tune(sfx26)
+samples18 = make_tune(sfx18, 2 * default_samples_per_note)
 
-combined = [x[0] + x[1] for x in zip(samples17, samples26)]
+#combined = [x[0] + x[1] for x in zip(samples17, samples26)]
+write_wav(samples_test1, "sfx_test1.wav")
 write_wav(samples17, "sfx17.wav")
-#write_wav(samples26, "sfx26.wav")
+write_wav(samples18, "sfx18.wav")
+write_wav(samples26, "sfx26.wav")
 #write_wav(combined, "combined.wav")
